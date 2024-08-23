ed707e542f931712b3039f697c536a0b52cb6873
Snehal Kadam <kadamss1912@gmail.com>
2024-08-23 12:07:08 +0530

    Add files via upload

---
+++ python_day2_operator_assigment3.py
@@ -0,0 +1,8 @@
+#3. Python program to convert the temperature in degree centigrade to Fahrenheit
+
+celsius=45
+fahrenheit = (celsius * 9/5) + 32
+print(celsius, "is convert to", fahrenheit)
+
+
+#ANS=>> 45 degrees Celsius is converted to 113.0 degrees Fahrenheit
