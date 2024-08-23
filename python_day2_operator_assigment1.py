48748ac731ef5f50360ad665846ab0942b6f4f44
Snehal Kadam <kadamss1912@gmail.com>
2024-08-23 12:06:16 +0530

    Add files via upload

---
+++ python_day2_operator_assigment1.py
@@ -0,0 +1,12 @@
+#1. Calculate the multiplication and sum of two numbers
+
+a=5
+b=6
+Multiplication =a*b
+Summation=a+b
+print(Multiplication)
+print(Summation)
+#ANS=>> Multiplication=30 and Summation=11
+
+
+
