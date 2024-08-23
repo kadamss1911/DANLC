7fa64a2100023792a25ec25dd7bfd92bca47d415
Snehal Kadam <kadamss1912@gmail.com>
2024-08-23 12:06:37 +0530

    Add files via upload

---
+++ python_day2_operator_assigment2.py
@@ -0,0 +1,8 @@
+# 2. Declare two variables and print that which variable is largest using ternary operators
+
+class_a=80
+class_b=100
+highest_count= class_a if class_a > class_b else class_b
+print( highest_count)
+
+#ANS=>> 100
